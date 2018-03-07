c47e29c3b1895974f4ba9897fd6b4fb20d9d2ecc
padrisimo <chamizho@gmail.com>
2018-03-07 20:04:47 +0100

    using the async/await fecth API

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import {  View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ajax from '../ajax';
+import DealList from './DealList';
 
 export default class App extends Component {
+  state = {
+    deals: []
+  }
+  async componentDidMount() {
+    const deals = await ajax.fetchInitialDeals();
+    this.setState({ deals });
+
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}> Bakesale </Text>
+        {this.state.deals.length > 0 ? (
+          <DealList deals={this.state.deals} />
+        ) : (
+          <Text style={styles.header}> Bakesale </Text>
+        )}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    header: {
-        fontSize: 40
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 40
+  }
 })
