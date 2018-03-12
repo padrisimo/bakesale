e8d7538720a87c347df29347be2387e1581b0ec5
padrisimo <chamizho@gmail.com>
2018-03-12 15:59:40 +0100

    fetching more API data for a view

--- src/ajax.js
+++ src/ajax.js
@@ -3,13 +3,24 @@ const apiHost = 'https://bakesaleforgood.com';
 export default {
   async fetchInitialDeals() {
     try {
-      let response = await fetch(
+      const response = await fetch(
         `${apiHost}/api/deals`
       );
-      let responseJson = await response.json();
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async fetchDealDetail(dealId) {
+    try {
+      const response = await fetch(
+        `${apiHost}/api/deals/${dealId}`
+      );
+      const responseJson = await response.json();
       return responseJson;
     } catch (error) {
       console.error(error);
     }
   }
-}
+};
